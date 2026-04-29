ff5df211c2732dee1b42e6be5d9bbc6d68c58adc
huythinh1223-svg <huythinh1223@gmail.com>
2026-04-29 14:55:32 +0700

    sua class CloseAuctionException

--- common/src/main/java/Auction/example/exception/CloseAuctionException.java
+++ common/src/main/java/Auction/example/exception/CloseAuctionException.java
@@ -1,11 +1,7 @@
-package user.code.common.src.main.java.Auction.example.exception;
+package Auction.example.exception;
 
 public class CloseAuctionException extends RuntimeException {
     public CloseAuctionException(String reason, String id) {
         super(reason);
     }
-
-
-
-
 }
